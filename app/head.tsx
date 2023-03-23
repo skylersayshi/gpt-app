aa857be6357a5aef7c8a73ba57ad989c5a002c7b
giantnerd123 <payneskyler@yahoo.com>
2023-03-22 19:35:08 -0700

    title changes

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,8 @@ function Head() {
         <title>ChatGPT | Skyler</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/images/logo.png" />
+        <link rel="apple-touch-icon" href="/images/logo.png" />
+        <meta name="description" content="Website clone using Next.js and Firebase" />
     </>
   )
 }
